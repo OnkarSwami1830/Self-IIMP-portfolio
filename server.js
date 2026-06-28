e77650119aeabcfb7cae1b2aa2ccb2fcb2fb04a9
Onkar Swami <onkarswami92@gmail.com>
2026-06-28 21:06:22 +0530

    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'OM.HTML')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 const openaiKey = process.env.OPENAI_API_KEY;
 const openai = openaiKey ? new OpenAI({ apiKey: openaiKey }) : null;
